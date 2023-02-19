40fd010470b03aebbaa85f32a9f211d42f7d289e
John Lee <codebeast0420@gmail>
2023-02-18 19:25:45 -0800

    Initial Commit

---
+++ Jerry/JerryApp.swift
@@ -0,0 +1,17 @@
+//
+//  JerryApp.swift
+//  Jerry
+//
+//  Created by Adam Sims on 2/18/23.
+//
+
+import SwiftUI
+
+@main
+struct JerryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
